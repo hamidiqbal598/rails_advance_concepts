ff15d1b870ce9a8cffdafde40b2e584531eff133
Hamid Iqbal <hamidiqbal598@gmail.com>
2023-04-01 22:24:55 +0200

    Report Worker added

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: bundle exec puma -C config/puma.rb
+
+reportWorker:bundle exec sidekiq -c 1
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => "/sidekiq"
+
+  resources :reports, only: %i[index] do
+    collection do
+      get :click_generate
+    end
+  end
+
 end
